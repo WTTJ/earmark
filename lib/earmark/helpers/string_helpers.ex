bfadde9ab32c0702a1a73a59d52ac7a7de2b8b5a
Guillaume Pouilloux <guillaume.pouilloux@wttj.co>
2024-02-08 22:56:16 +0100

    fix(stringHelper): replace slice by split_at

--- lib/earmark/helpers/string_helpers.ex
+++ lib/earmark/helpers/string_helpers.ex
@@ -1,12 +1,12 @@
 defmodule Earmark.Helpers.StringHelpers do
-
   @moduledoc false
 
   @doc """
   Remove the leading part of a string
   """
   def behead(str, ignore) when is_integer(ignore) do
-    String.slice(str, ignore..-1)
+    {_pre, post} = String.split_at(str, ignore)
+    post
   end
 
   def behead(str, leading_string) do
